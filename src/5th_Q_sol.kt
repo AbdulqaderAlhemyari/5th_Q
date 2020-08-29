3595a3d31b7a1fef1ec58692d3e02ece020bfa67
Abdulqader <alhemyariabdulqader@gmail.com>
2020-08-30 00:14:35 +0300

    sol of 5th Q

---
+++ src/5th_Q_sol.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+    val list = listOf(8,4,7,1,2,3,0,5,6)
+
+    println("the ascending order is:  " + list.sorted())
+
+    println("the descending order is:  " + list.sortedDescending())
+}
